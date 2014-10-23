9950c3352b15481ff6eaf42b0a624fbd1df15473
jasonpmcculloch <jason.mcculloch@gmail.com>
2014-10-23 22:56:12 +1300

    Initial non-working version

---
+++ MacDevelopments/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MacDevelopments.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Demolition()
+        {
+            return View();
+        }
+
+        public ActionResult Construction()
+        {
+            return View();
+        }
+
+        public ActionResult Contracting()
+        {
+            return View();
+        }
+    }
+}
